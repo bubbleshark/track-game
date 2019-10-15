e7b78197cf0a478a20e02ea7d25d29ff8939e840
Billy Brian <goodthing61@gmail.com>
2019-10-16 01:37:21 +0800

    game.py finished

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ white_color = (255, 255, 255, 0)
 width, height = 800, 600
 fps = 30
 user_input = True
-player_num = 2
+player_num = 1
 player_info={
     "rotate_step": 3,
     "initial_x": 121,
@@ -30,9 +30,9 @@ player_info={
 def main():
     game = TrackGameHelper(map_file,obstacle_color,white_color,width,height,fps)
     game.set_player(car_file,car_file2,player_info,player_num)
-    #game.run()
-    #'''
-    player_input_list = [[True,False,False,False] for player in game.player]
+    game.run()
+    '''
+    player_input_list = [[False,False,False,False] for player in game.player]
     while True:
         for e in pygame.event.get():
                 if e.type == pygame.QUIT:
@@ -45,15 +45,16 @@ def main():
                     pygame.quit()
                     exit()
                 if e.type == pygame.KEYDOWN and e.key == pygame.K_r:
-                    player_input_list = [[True,False,False,False] for player in game.player]
+                    player_input_list = [[False,False,False,False] for player in game.player]
                     for player in game.player:
                         player.reset()
         game.run_step(player_input_list)
         # for bot control
+        
         for player in game.player:
             print(player.sensor_value)
         player_input_list= [None for player in game.player]
         for i,e in enumerate(player_input_list):
             player_input_list[i]= [True,False,False,False]
-        
+        '''
 main()
